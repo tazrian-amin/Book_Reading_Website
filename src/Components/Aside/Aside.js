c85ec18dd74caf4394b9b3f2668a29fb5ad5fcb5
tazrian-amin <amin.tazrian1979@gmail.com>
2022-09-29 21:27:23 +0600

    aside structure completed

--- src/Components/Aside/Aside.js
+++ src/Components/Aside/Aside.js
@@ -2,39 +2,51 @@ import React from 'react';
 import './Aside.css';
 const Aside = () => {
     return (
-        <div className='container bg-white'>
-            <h3 className='text-center fw-bold text-success'>Reader Info</h3>
+        <div className='container bg-white aside h-100'>
+            <h3 className='text-center fw-bold pt-3'>Reader Info</h3>
             <div className='container d-flex align-items-center justify-content-between bg-secondary bg-opacity-25 pt-3 text-center border border-light rounded'>
                 <div>
-                    <h6>Name</h6>
+                    <h6 className='fw-bold'>Name</h6>
                     <p>Tazrian</p>
                 </div>
                 <div>
-                    <h6>Age</h6>
+                    <h6 className='fw-bold'>Age</h6>
                     <p>24</p>
                 </div>
                 <div>
-                    <h6>Location</h6>
+                    <h6 className='fw-bold'>Location</h6>
                     <p>Bangladesh</p>
                 </div>
             </div>
-            <h5 className='my-3'>Add An Interval</h5>
+            <h5 className='my-3 text-success fw-bold'>Add An Interval</h5>
             <div className='interval-btn container py-3 d-flex align-items-center justify-content-between bg-secondary bg-opacity-25 text-center border border-light rounded'>
                 <div>
-                    <button className='border-0 rounded-circle p-1'>2hr</button>
+                    <button className='border-0 rounded-circle fw-bold p-1'>2hr</button>
                 </div>
                 <div>
-                    <button className='border-0 rounded-circle p-1'>3hr</button>
+                    <button className='border-0 rounded-circle fw-bold p-1'>3hr</button>
                 </div>
                 <div>
-                    <button className='border-0 rounded-circle p-1'>4hr</button>
+                    <button className='border-0 rounded-circle fw-bold p-1'>4hr</button>
                 </div>
                 <div>
-                    <button className='border-0 rounded-circle p-1'>5hr</button>
+                    <button className='border-0 rounded-circle fw-bold p-1'>5hr</button>
                 </div>
                 <div>
-                    <button className='border-0 rounded-circle p-1'>6hr</button>
+                    <button className='border-0 rounded-circle fw-bold p-1'>6hr</button>
                 </div>
+            </div>
+            <h5 className='my-3 text-success fw-bold'>Reading Details</h5>
+            <div className='d-flex align-items-center justify-content-between p-2 bg-secondary bg-opacity-25 border border-light rounded'>
+                <h6 className='fw-bold'>Total Pages to Read</h6>
+                <h6 id='total-pages-container'>0</h6>
+            </div>
+            <div className='d-flex align-items-center justify-content-between p-2 bg-secondary bg-opacity-25 border border-light rounded mt-2 mb-4'>
+                <h6 className='fw-bold'>Interval</h6>
+                <h6 id='interval'>0 hours</h6>
+            </div>
+            <div>
+                <a href="/" className='w-100 btn btn-success fw-bold'>Activity Completed</a>
             </div>
         </div>
     );
--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -6,14 +6,14 @@ const Card = (props) => {
         <div>
             <div className="col">
                 <div className="card">
-                    <img src={img} className="card-img-top" alt="..." />
+                    <img src={img} className="mt-2 w-50 mx-auto rounded" alt="..." />
                     <div className="card-body">
                         <h4 className="card-title fw-bold">{name}</h4>
                         <p className="card-text">{detail.slice(0, 150)} <span className='fw-bold'>...see more</span></p>
-                        <h6>Pages: {pages}</h6>
+                        <h6 className='fw-bold'>Pages: {pages}</h6>
                     </div>
                     <div className="card-footer">
-                        <a href="/" className='card-btn w-100 btn btn-success fw-bold'>Add to List</a>
+                        <a href="/" className='w-100 btn btn-success fw-bold'>Add to List</a>
                     </div>
                 </div>
             </div>
